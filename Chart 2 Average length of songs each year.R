3ac74820d0634680e841639d1aaaf4920e995c2b
junemih <121904610+junemih@users.noreply.github.com>
2023-02-20 23:57:37 -0800

    Update Chart 2 Average length of songs each year.R

--- Chart 2 Average length of songs each year.R
+++ Chart 2 Average length of songs each year.R
@@ -1,14 +1,5 @@
 # This chart shows the length of songs throughout the years. The happier people were the longer songs would be.
 
-# Throughout the years, the average length of songs increase and decrease based off of what and how much people
-# had to say. The most noticeable year is 1994 which was arguably one of the best years of music of all time. This is because
-# this was the start of pre dot com, people were happy, the state of the economy was recovering from the early 1990's recession
-# it was a great year for movies, and there was a lot to talk about. Spikes in average song length can also be closely correlated 
-# to happy song mood as well. In cases where happy songs had the highest mean valance, song lengths typically had longer times as 
-# well. A case study that analyzed conversations among 20,000 people found that happy people tend to be more talkative. This coloration
-# was found based off people who were happier having not only longer conversations but less small chat and more substantive conversations.
-# This study helps show the linkage between peoples emotions and moods playing a big role on how much they talk and say or in this case, thier music. 
-
 # Citation: 
 # Eavesdropping on happiness: Well-being is related to ... - sage journals. (n.d.). Retrieved February 21, 2023, from https://journals.sagepub.com/doi/abs/10.1177/0956797610362675?journalCode=pssa 
 
